6aa8b0eb48168c30f979d8ce01ac0b72ad286861
Jon <jon.bosque.hernando@gmail.com>
2018-05-17 19:27:00 +0200

    implement heat functionality

--- src/geme.adb
+++ src/geme.adb
@@ -2,36 +2,40 @@ with Ada.Text_IO;
 with Ada.Float_Text_IO;
 with menu;
 with mockup_interface;
+with mockup_control;
 
 
 procedure geme is
       opcion: Character;
+      power: Float;
 begin
 
-   opcion := menu.menu_impl;
-
-   case opcion is
-      when '1' =>
-            Ada.Text_IO.Put("Introduzca potencia de calentado (Wats):  ");
-            --mockup_interface.heat(Ada.Float_Text_IO.Get); -- feed user input to heat()
-      when '2' =>
-            mockup_interface.show_temp;
-         null;
-      when '3' =>
-            mockup_interface.read_save_temp;
-         null;
-      when '4' =>
-            mockup_interface.show_array;
-         null;
-      when '5' =>
-            mockup_interface.show_avg_temp;
-      when '6' =>
-            --mockup_interface.show_overlevel
-         null;
-      when '7' =>
-            mockup_interface.close_mockup;
-      when others =>
+      loop--
+      opcion := menu.menu_impl;
+      case opcion is
+            when '1' =>
+                  Ada.Text_IO.Put("Introduzca potencia de calentado (Wats):  ");
+                  Ada.Float_Text_IO.Get(power);
+                  mockup_control.heat(power); -- feed user input to heat()
+            when '2' =>
+                  mockup_interface.show_temp;
+            when '3' =>
+                  mockup_interface.read_save_temp;
             null;
-      end case;
+            when '4' =>
+                  mockup_interface.show_array;
+            null;
+            when '5' =>
+                  mockup_interface.show_avg_temp;
+            when '6' =>
+                  --mockup_interface.show_overlevel
+            null;
+            when '7' =>
+                  mockup_control.close;
+            when others =>
+                  null;
+            end case;
+      exit when opcion='7';
+      end loop;
 
 end geme;
