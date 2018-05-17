4c403f9ec544f5146baae11df4e0d8d2d255ed4e
Jon <jon.bosque.hernando@gmail.com>
2018-05-17 23:27:42 +0200

    Add length check

--- src/manejo_buffer.adb
+++ src/manejo_buffer.adb
@@ -1,9 +1,26 @@
+with Ada.Text_IO;
+
 package body manejo_buffer is
+    max_length: Integer:= -1;
+
+    procedure Initialize(max_len:Integer) is
+    begin
+        max_length := max_len;
+    end Initialize;
+
 
    procedure guardar_dato (elem_buffer: tipo_generico; array_buffer: out t_array_buffer) is
    begin
-      long_buffer:=long_buffer+1;
-      array_buffer(long_buffer):=elem_buffer;
+        if max_length=-1 then
+            Ada.Text_IO.Put_Line("Buffer no inicializado!");
+            --rajse Error
+        end if;
+        if long_buffer<max_length then
+            long_buffer:=long_buffer+1;
+        else
+            Ada.Text_IO.Put_Line("Atencion! El buffer esta lleno!");
+        end if; 
+        array_buffer(long_buffer):=elem_buffer;
 
    end guardar_dato;
 
--- src/manejo_buffer.ads
+++ src/manejo_buffer.ads
@@ -2,9 +2,10 @@ generic
 
    type tipo_generico is private;
    type t_array_buffer is array (Integer range <>) of tipo_generico;
-package manejo_buffer is
 
+package manejo_buffer is
    long_buffer:Natural:=0;
+   procedure initialize(max_len: Integer);
    procedure guardar_dato (elem_buffer: tipo_generico; array_buffer: out t_array_buffer);
    procedure resetear_buffer;
 end manejo_buffer;
