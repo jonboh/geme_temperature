85bdb13cc1df8aedf1d48b9e618b9521da1c50c8
Jon <jon.bosque.hernando@gmail.com>
2018-05-17 19:18:46 +0200

    Add messages at entry

--- src/pcm3712_mock.adb
+++ src/pcm3712_mock.adb
@@ -1,11 +1,15 @@
+with Ada.Text_IO;
+with Ada.Float_Text_IO;
+
 package body PCM3712_mock is
     procedure Initialize_PCM3712 is
     begin
-        null;
+        Ada.Text_IO.Put_Line("Initializing PCM3712");
     end Initialize_PCM3712;
 
     procedure Write_ao_PCM3712(Volts: Float) is
     begin
-        null;
+        Ada.Text_IO.Put("Heating with ");
+        Ada.Float_Text_IO.Put(Volts);
     end Write_ao_PCM3712;    
 end PCM3712_mock;
