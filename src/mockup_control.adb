0d891f964108d815eb92d8ec0bd61c13d52bb61b
Jon <jon.bosque.hernando@gmail.com>
2018-05-24 17:05:37 +0200

    Use drivers instead of mock packages

--- src/mockup_control.adb
+++ src/mockup_control.adb
@@ -1,31 +1,43 @@
-with PCM3712_mock;
-with PCM3718_mock;
-
+with driver_pcm3712_ao_ehu; use driver_pcm3712_ao_ehu;
+with driver_pcm3718_ai; use driver_pcm3718_ai;
+with Ada.Text_IO; use Ada.Text_IO;
 
 package body mockup_control is
 
-    gain_wats_volt: constant Float := 80.0;
+   gain_wats_volt: constant Float := 80.0;
+   gain_volt_temp: constant Float := 20.0;
 
     procedure heat(power: Float) is
-    volts: Float;
-    begin
-        volts := power * gain_wats_volt;
-        PCM3712_mock.Write_ao_PCM3712(volts);
+      volts: Float;
+      power_aux: Float;
+      begin
+         power_aux := power;
+      if power > 350.0 then
+         power_aux := 350.0;
+         Put_Line("Potencia Limitada a 350 W");
+ 	end if;
+      if power < 50.0 then
+          power_aux := 50.0;
+          Put_Line("Potencia Minima 50 W");
+      end if;
+        volts := power_aux / gain_wats_volt;
+        Write_ao_PCM3712(volts);
     end heat;
 
     function read_temp return Float is
     temp: Float;
     begin
-        temp := PCM3718_mock.Adquirir;
+        temp := driver_pcm3718_ai.Adquirir * gain_volt_temp;
     return temp;
     end read_temp;
 
     procedure close is
     begin
-        PCM3718_mock.Fin_Adquisicion;
+        Fin_Adquisicion;
     end close;
-    
+
 begin
-    PCM3712_mock.Initialize_PCM3712;
-    --PCM3718_mock.Configuracion_Inicial()
+    Initialize_PCM3712;
+    Configuracion_Inicial(Conexion => Differential_Ended, Canal_Primero => 0, Canal_Ultimo=>0, Disparo => Software, Numero_muestras => 1, Rango => Unipolar_5);
+    heat(50.0);
 end mockup_control;
--- src/mockup_interface.adb
+++ src/mockup_interface.adb
@@ -1,14 +1,14 @@
-with Ada.Text_IO; 
+with Ada.Text_IO;
 with Ada.Float_Text_IO;
 with mockup_control;
 with manejo_buffer;
 
-package body mockup_interface is 
+package body mockup_interface is
     max_length_buffer: constant Integer:= 25;
     type temp_array_type is array(Integer range <>) of Float;
     temps : temp_array_type(0..max_length_buffer);
     package buffer_temp is new manejo_buffer(Float,temp_array_type);
-   
+
     procedure show_temp is
     temp: Float;
     begin
@@ -23,14 +23,14 @@ package body mockup_interface is
       temp := mockup_control.read_temp;
       buffer_temp.guardar_dato(temp, temps);
     end read_save_temp;
-    
+
     procedure show_array is
     length: Integer;
     begin
         Ada.Text_IO.Put_line("Array Temperatura: ");
         length := buffer_temp.long_buffer;
         for i in 1..length loop
-            Ada.Float_Text_IO.Put(temps(i),0,2,0);Ada.Text_IO.New_Line; 
+            Ada.Float_Text_IO.Put(temps(i),0,2,0);Ada.Text_IO.New_Line;
         end loop;
     end show_array;
 
@@ -42,14 +42,14 @@ package body mockup_interface is
       length := buffer_temp.long_buffer;
       F_length:= Float(length);
       for i in 1..length loop
-            F_temp_aux:=temps(i)+F_temp_aux; 
+            F_temp_aux:=temps(i)+F_temp_aux;
       end loop;
       F_temp_media:=F_temp_aux/F_length;
       Ada.Text_IO.Put_line("Temperatura media: ");
-      Ada.Float_Text_IO.Put(F_temp_media);Ada.Text_IO.New_Line; 
+      Ada.Float_Text_IO.Put(F_temp_media,0,2,0);Ada.Text_IO.New_Line;
         null;
    end show_avg_temp;
-   
+
    procedure show_overlevel(F_umbral: in Float) is
       length:Integer;
       F_count,F_length,F_percen:Float;
@@ -61,17 +61,19 @@ package body mockup_interface is
       for i in 1..length loop
          if temps(i)>F_umbral then
             Ada.Text_IO.Put_line(" ");
-            Ada.Float_Text_IO.Put(temps(i));
+            Ada.Float_Text_IO.Put(temps(i),0,2,0);
             F_count:=F_count+1.0;
          else
             null;
          end if;
-         
+
       end loop;
-      Ada.Text_IO.New_Line; 
+      Ada.Text_IO.New_Line;
       F_percen:=(F_count*100.0)/F_length;
       Ada.Text_IO.Put_line("El porcentaje de temperaturas que superan el umbral es: ");
-      Ada.Float_Text_IO.Put(F_percen);Ada.Text_IO.New_Line; 
+      Ada.Float_Text_IO.Put(F_percen,0,2,0);
+      Ada.Text_IO.Put(" 0/0");
+      Ada.Text_IO.New_Line;
       null;
       end show_overlevel;
 
